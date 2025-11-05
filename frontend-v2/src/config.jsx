0aca7555e1d83bfd95c342aef04a4718e3e9606a
Dhruvermafz <vermadhruv09112002@gmail.com>
2025-11-05 20:25:19 +0530

    aded vite frontend and backend

--- frontend-v2/src/config.jsx
+++ frontend-v2/src/config.jsx
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://social-api-w6xb.onrender.com/"
+    ? "https://itsablog-api.onrender.com/"
     : "http://localhost:5000/";
